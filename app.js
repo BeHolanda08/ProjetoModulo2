2160cea712d98a59bb97e9f6930ebd2bb8c6db97
Gledson Holanda <holanda.be@gmail.com>
2019-09-17 22:02:19 -0300

    [Onboard]{Gledson/Ricardo} -Rota Index

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Candidate = require('./models/Candidate');
 const Company = require('./models/Company');
+const bcrypt = require('bcrypt');
 
 // const session = require('express-session');
 const app = express();
 
+const saltRounds = 10;
+
 // MongoBD Atlas
 
 const url = process.env.DB_HOST;
-const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true };
+const options = {
+  reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true };
 
 // Mongoose
 
@@ -40,6 +44,31 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.post('/', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.render('index', { 
+      errorMessage: 'Por favor, preencha todos os campos obrigatórios!' });
+    return;
+}
+
+  const candidate = await Candidate.findOne({ email, password });
+  const company = await Company.findOne({ email, password });
+
+
+  if (!candidate) {
+    console.log(candidate);
+    res.render('index', { errorMessage: "The username doesn't exist." });
+    return;
+  }
+  if (bcrypt.compareSync(password, candidate.password)) {
+    // req.session.currentUser = candidate;
+    res.redirect('/home');
+  } else {
+    res.render('/', { errorMessage: 'Incorrect password' });
+  }
+});
+
 app.get('/signUp', (req, res) => {
   res.render('signUp');
 });
@@ -55,17 +84,23 @@ app.get('/signUpCandidate', (req, res) => {
 app.post('/signUpCandidate', async (req, res) => {
   const { name, email, password, surname, celPhone } = req.body;
 
-  // validação de candidato
-  if (name === '' || email === '' || password === '' || surname === '' || celPhone === '') {
-    res.render('signupCandidate', { errorMessage: 'Please fill all required fields!' });
+  // validação de candidate
+  if (!name || !email || !password || !surname || !celPhone) {
+    return res.send('signupCandidate', { errorMessage: 'Por favor, preencha todos os campos obrigatórios!' });
   }
 
-  const newCandidate = new Candidate({ name, email, password, surname, celPhone });
+  if (await Candidate.findOne({ email })) {
+    return res.send({ error: 'Usuário já cadastrado!' });
+  }
+
   try {
-    await newCandidate.save(); 
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    const newCandidate = new Candidate({ name, email, password: hash, surname, celPhone });
+    await newCandidate.save();
     res.redirect('/');
   } catch (error) {
-    console.log(error);
+    console.log(error, 'Erro de Login!');
   }
 });
 
@@ -77,18 +112,23 @@ app.get('/signUpCompany', (req, res) => {
 app.post('/signUpCompany', async (req, res) => {
   const { name, phone, email, password } = req.body;
 
-  // validação de candidato
-  if (name === '' || phone === '' || email === '' || password === '') {
-    res.render('signUpCompany', { errorMessage: 'Please fill all required fields!' });
+  // validação de company
+  if (!name || !phone || !email || !password) {
+    res.send('signUpCompany', { errorMessage: 'Por favor, preencha todos os campos Obrigatórios!' });
   }
 
-  const newCompany = new Company({ name, phone, email, password });
+  if (await Company.findOne({ email })) {
+    return res.send({ error: 'Empresa já cadastrado!' });
+  }
 
   try {
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    const newCompany = new Company({ name, phone, email, password: hash });
     await newCompany.save();
     res.redirect('/');
   } catch (error) {
-    console.log(error);
+    console.log(error, 'Erro de Login!');
   }
 });
 
@@ -96,9 +136,7 @@ app.get('/forgotPassword', (req, res) => {
   res.render('forgotPassword');
 });
 
-app.post('/forgotPassword', (req, res) => {
-  return res.render('forgotPassword', { errorMessage: 'Please fill all required fields!' });
-});
+app.post('/forgotPassword', (req, res) => res.render('forgotPassword', { errorMessage: 'Please fill all required fields!' }));
 
 app.get('/passwordSubmission', (req, res) => {
   res.render('passwordSubmission');
